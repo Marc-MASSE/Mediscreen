b5a3df6ed9348d5fd26221a1faa7e77730480d37
Marc MASSE <marc.masse600@orange.fr>
2023-03-10 11:18:20 +0100

    Patientui project added

---
+++ patientui/src/main/java/fr/marc/patientui/PatientuiApplication.java
@@ -0,0 +1,13 @@
+package fr.marc.patientui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientuiApplication.class, args);
+	}
+
+}
