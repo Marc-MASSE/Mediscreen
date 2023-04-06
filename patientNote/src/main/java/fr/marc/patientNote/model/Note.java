b7171fa209c7eda4759e38ccc0e543251e39b621
Marc MASSE <marc.masse600@orange.fr>
2023-04-06 11:29:48 +0200

    Update notes functionality done

--- patientNote/src/main/java/fr/marc/patientNote/model/Note.java
+++ patientNote/src/main/java/fr/marc/patientNote/model/Note.java
@@ -18,21 +18,21 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Document(collection = "notes")
 public class Note {
-	
+
 	@Id
 	private String id;
-	
+
 	private Integer patId;
-	
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
-    private LocalDateTime date;
-	
-    private String body;
-    
-    public Note(Integer patId,LocalDateTime date, String body) {
-        this.patId = patId;
-        this.date = date;
-        this.body = body;
-      }
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private LocalDateTime date;
+
+	private String body;
+
+	public Note(Integer patId, LocalDateTime date, String body) {
+		this.patId = patId;
+		this.date = date;
+		this.body = body;
+	}
 
 }
--- patientui/src/main/java/fr/marc/patientui/controller/NoteController.java
+++ patientui/src/main/java/fr/marc/patientui/controller/NoteController.java
@@ -5,9 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import fr.marc.patientui.beans.NoteBean;
+import fr.marc.patientui.beans.PatientBean;
+import fr.marc.patientui.proxies.PatientInfoProxy;
 import fr.marc.patientui.proxies.PatientNoteProxy;
 
 @Controller
@@ -15,9 +19,11 @@ public class NoteController {
 	
 	private static final Logger log = LoggerFactory.getLogger(NoteController.class); 
 	private final PatientNoteProxy patientNoteProxy;
+	private final PatientInfoProxy patientInfoProxy;
 
-	public NoteController(PatientNoteProxy patientNoteProxy) {
+	public NoteController(PatientNoteProxy patientNoteProxy, PatientInfoProxy patientInfoProxy) {
 		this.patientNoteProxy = patientNoteProxy;
+		this.patientInfoProxy = patientInfoProxy;
 	}
 	
 	@GetMapping("/NoteList")
@@ -27,7 +33,7 @@ public class NoteController {
 	{
 		log.info("Note Attributes: {}", patientNoteProxy.getNotesByPatientId(patId));
 		model.addAttribute("notes", patientNoteProxy.getNotesByPatientId(patId));
-		model.addAttribute("patientId",patId);
+		model.addAttribute("patient",patientInfoProxy.getPatientById(patId));
 		return "NoteList";
 	}
 	
@@ -35,7 +41,20 @@ public class NoteController {
 	public String noteUpdatePage(@RequestParam String id, Model model){
 		NoteBean note = patientNoteProxy.getNoteById(id).get();
 		model.addAttribute("note",note);
+		PatientBean patient = patientInfoProxy.getPatientById(note.getPatId());
+		model.addAttribute("patient", patient);
 		return "NoteUpdate";
 	}
+	
+	@PostMapping("/NoteUpdate")
+	public String noteUpdateValidation(
+			@RequestParam String id,
+			@ModelAttribute("note") NoteBean note) 
+	{
+		NoteBean updatedNote = patientNoteProxy.updateNote(id, note);
+		log.info("Note Attributes: {}",updatedNote);
+		return "redirect:/NoteList?patId=" + updatedNote.getPatId();
+	}
+	
 
 }
