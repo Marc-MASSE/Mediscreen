3e22a315c80d13914cb9cb1c6f000081bdffa907
Marc MASSE <marc.masse600@orange.fr>
2023-04-20 15:17:37 +0200

    Ajout des fichiers Jacoco pour patientInfo

---
+++ patientReport/target/classes/data.sql
@@ -0,0 +1,19 @@
+/* Data to test the PatientReport DB */
+
+USE mediscreen;
+
+-- Default values for table 'triggers'
+
+INSERT INTO triggers (term)
+VALUES
+('hémoglobine a1c'),('hemoglobin a1c'),
+('microalbumine'),('microalbumin'),
+('taille'),('height'),
+('poids'),('weight'),
+('fumeur'),('smoker'),
+('anormal'),('abnormal'),
+('cholestérol'),('cholesterol'),
+('vertige'),('dizziness'),
+('rechute'),('relapse'),
+('réaction'),('reaction'),
+('anticorps'),('antibodies');
